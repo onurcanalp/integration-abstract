d0c779052f4a590c40b156fdc32ac1f603582e65
Onur <onurcanalp@siyyah.com>
2015-01-27 10:33:18 +0200

    Emsal sınıf
    Örneğin lidyana entegrasyonu yapacaksanız buyrun örnek sınıf size

---
+++ classLidyana.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Lidyana Entegrasyon Sınıfımız
+ * User: Onur CANALP
+ * Date: 19/01/15
+ * Time: 11:15
+ */
+
+require("abstractClassIntegration.php");
+
+class Lidyana extends Integration
+{
+    public function xml2Array()
+    {
+        
+    }
+
+    public static function stockCount($product_id = 0)
+    {
+        
+    }
+
+    public static function saveOrderToLidyana($products = array())
+    {
+        
+    }
+}
